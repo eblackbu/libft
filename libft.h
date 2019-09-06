9a561319dc92cd0448d67cbed2c0f07c6b40b69d
Eblackbu <romden122000@yandex.ru>
2019-09-06 09:37:42 +0300

    list

--- libft.h
+++ libft.h
@@ -5,6 +5,13 @@
 # include <stdlib.h>
 # include <string.h>
 
+typedef struct		s_list
+{
+	void			*content;
+	size_t			content_size;
+	struct s_list	*next;
+}					t_list;
+
 size_t  ft_strlen(const char *str);
 char	*ft_strrchr(const char *str, int c);
 char	*ft_strstr(const char *s1, const char *s2);
@@ -43,5 +50,11 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char const *s, int fd);
 void	ft_putendl_fd(char const *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
+t_list	*ft_lstnew(void const *content, size_t content_size);
+void	ft_lstdelone(t_list **alst, void(*del)(void *, sizze_t));
+void	ft_lstdel(t_list *alst, void(*del)(void *, size_t));
+void	ft_lstadd(t_list **alst, t_list *new);
+void	ft_lstiter(t_list *lst, void(*f)(t_list *elem));
+
 
 # endif
