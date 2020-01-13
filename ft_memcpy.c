d7d2674da21c22fea125cc8eb45f375fada8918f
root <root>
2020-01-13 09:10:05 +0300

    ft_memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -15,15 +15,14 @@
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	size_t	i;
+	unsigned char		*d;
+	unsigned char		*s;
 
-	i = 0;
 	if (!(src || dest))
 		return (NULL);
-	while (i < n)
-	{
-		((unsigned char*)dest)[i] = ((unsigned char*)src)[i];
-		i++;
-	}
+	d = (unsigned char*)dest;
+	s = (unsigned char*)src;
+	while (n--)
+		*d++ = *s++;
 	return (dest);
 }
